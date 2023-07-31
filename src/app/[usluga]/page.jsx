03e50abfde3d05c564e05fe5133530f4d5f13a5b
Zjapa <japax97@gmail.com>
2023-07-31 21:24:15 +0200

    Creating template for pages

--- src/app/[usluga]/page.jsx
+++ src/app/[usluga]/page.jsx
@@ -1,19 +1,45 @@
 import Banner from '@/components/Banner/Banner';
 import React from 'react';
 import { usluge } from './usluge-const';
+import Image from 'next/image';
+import './page.scss';
 
 const page = ({ params }) => {
-    const { usluga } = params;
-    const { title, img, text, sideList } = usluge[usluga];
-    return (
-        <section className='service-page-item'>
-            <Banner title={title} />
-            <div className="container service-page-item-wrapper">
-                <div className="service-page-item-top"></div>
-                <div className="service-page-item-text"></div>
+  const { usluga } = params;
+  const { pageTitle, img, imgSecond, text, title, titleSecond, sideList } = usluge[usluga];
+  return (
+    <section className="service-page-item">
+      <Banner title={pageTitle} />
+      {/* <span className="sidebar">
+        {sideList.map((item) => (
+          <p key={item}>{item}</p>
+        ))}
+      </span> */}
+      <div className="container service-page-item-wrapper">
+        <div className="service-page-item-top">
+          <Image src={img} alt={pageTitle} className="service-page-item-image" />
+          <div className="text-container">
+            <h2 className="service-page-item-subtitle">{title}</h2>
+            <p className="desc-text">{text[0]}</p>
+            <p className="desc-text">{text[1]}</p>
+          </div>
+        </div>
+        <div className="service-page-item-top">
+          <div className="text-container">
+            <h2 className="service-page-item-subtitle">{titleSecond}</h2>
+            <p className="desc-text">{text[2]}</p>
+            <div className="sidebar">
+              {sideList.map((item) => (
+                <p key={item}>{item}</p>
+              ))}
             </div>
-        </section>
-    );
+          </div>
+          <Image src={imgSecond} alt={pageTitle} className="service-page-item-image" />
+        </div>
+        <div className="service-page-item-text"></div>
+      </div>
+    </section>
+  );
 };
 
 export default page;
--- src/app/[usluga]/usluge-const.js
+++ src/app/[usluga]/usluge-const.js
@@ -1,26 +1,29 @@
-import camovoDrvoImg from '@/img/drveni_prozori/camovo-drvo.jpg'
-
+import camovoDrvoImg from '@/img/drveni_prozori/camovo-drvo.jpg';
+import camovoDrvoImg2 from '@/img/drveni_prozori/slika4.jpg';
 
 const CAMOVO_DRVO =
-    'Asortiman naših prozora je širok iz prostog razloga, jer delimično podržavamo argumente pristalica koje zastupaju svaki od ova četiri tipa stolarije, a pošto želimo da zadovoljimo svačiji ukus potrudili smo se da od prozora koje želite napravimo proizvod vrhunskog kvaliteta u svojoj klasi.';
+  'Asortiman naših prozora je širok iz prostog razloga, jer delimično podržavamo argumente pristalica koje zastupaju svaki od ova četiri tipa stolarije, a pošto želimo da zadovoljimo svačiji ukus potrudili smo se da od prozora koje želite napravimo proizvod vrhunskog kvaliteta u svojoj klasi.';
 
 const CAMOVO_DRVO_2 =
-    'Profil od kog se izradjuju Drveni prozori - čamovo drvo je od troslojno lameliranog drveta isključivo od prvoklasne gradje ( bez čvora ) namenjene za drvenu stolariju. Drvo kao najplemenitiji materijal pružiće vam mnogostruko više u odnosu u ono šta u njega uložite, uz blagovremeno održavanje prirodnošću i lepotom krasiće vaš dom i davati mu nezamenljivu toplinu. Hrast, bor ili čamovina ( jela i smreka ),mahagonij ili merante na vama je da se odlučite za vrstu drveta od kog će Vaši Drveni prozori - čamovo drvo biti izradjeni, a mi ćemo od njega napraviti proizvod koji odgovara najvišim svetskim standardima, prema kriterijumima koje treba da zadovolji drvena stolarija. Bila to toplotna izolacija, zvučna izolacija ili postojanost samog materijala kod drvenih prozora. Da bi u tome uspeli, koristimo niskoemisiono termo staklo 4-16-4, duplu dihtung gumu Deventer koja pruža toplotnu i zvučnu izolaciju, kao i Roto okove. Tip zatvaranja i otvaranja takodje sami birate : obično, okretno nagibno ( kip ), klizna vrata, kombi sistem, harmonika,klizna stena…';
+  'Profil od kog se izradjuju Drveni prozori - čamovo drvo je od troslojno lameliranog drveta isključivo od prvoklasne gradje ( bez čvora ) namenjene za drvenu stolariju. Drvo kao najplemenitiji materijal pružiće vam mnogostruko više u odnosu u ono šta u njega uložite, uz blagovremeno održavanje prirodnošću i lepotom krasiće vaš dom i davati mu nezamenljivu toplinu. Hrast, bor ili čamovina ( jela i smreka ),mahagonij ili merante na vama je da se odlučite za vrstu drveta od kog će Vaši Drveni prozori - čamovo drvo biti izradjeni, a mi ćemo od njega napraviti proizvod koji odgovara najvišim svetskim standardima, prema kriterijumima koje treba da zadovolji drvena stolarija. Bila to toplotna izolacija, zvučna izolacija ili postojanost samog materijala kod drvenih prozora. Da bi u tome uspeli, koristimo niskoemisiono termo staklo 4-16-4, duplu dihtung gumu Deventer koja pruža toplotnu i zvučnu izolaciju, kao i Roto okove. Tip zatvaranja i otvaranja takodje sami birate : obično, okretno nagibno ( kip ), klizna vrata, kombi sistem, harmonika,klizna stena…';
 
 const CAMOVO_DRVO_3 =
-    'Dokazano je da Drveni prozori - čamovo drvo imaju najbolju toplotnu i zvučnu izolaciju i za razliku od svih ostalih materijala, sposobnost difuzije pare. To je važno zbog mikroklime zatvorenog prostora. U spavaćoj sobi, kupatilu ili kuhinji, gdje se po osobi izbaci veća količina vode disanjem, znojenjem i utroškom vode, samo drveni prozori izbacuju putem difuzije tu preveliku količinu vlage, a u suprotnom slučaju, ubacuje potrebnu vlagu u prostor. Velika stabilnost drvene stolarije, poboljšana mehanička i fizička svojstva, postižu se lameliranjem, tj. Lepljenjem elemenata drveta, i njegovom termičkom obradom, tj. sušenjem na 12% sadržaja vode drvene građe, kojima pravilnom konstrukcijom postižemo maksimalnu stabilnost profila i bolja svojstva drvenog prozora. Drvo je najisplativija investicija po pitanju građevinske stolarije.';
+  'Dokazano je da Drveni prozori - čamovo drvo imaju najbolju toplotnu i zvučnu izolaciju i za razliku od svih ostalih materijala, sposobnost difuzije pare. To je važno zbog mikroklime zatvorenog prostora. U spavaćoj sobi, kupatilu ili kuhinji, gdje se po osobi izbaci veća količina vode disanjem, znojenjem i utroškom vode, samo drveni prozori izbacuju putem difuzije tu preveliku količinu vlage, a u suprotnom slučaju, ubacuje potrebnu vlagu u prostor. Velika stabilnost drvene stolarije, poboljšana mehanička i fizička svojstva, postižu se lameliranjem, tj. Lepljenjem elemenata drveta, i njegovom termičkom obradom, tj. sušenjem na 12% sadržaja vode drvene građe, kojima pravilnom konstrukcijom postižemo maksimalnu stabilnost profila i bolja svojstva drvenog prozora. Drvo je najisplativija investicija po pitanju građevinske stolarije.';
 
 export const usluge = {
-    'camovo-drvo': {
-        title: 'Čamovo drvo',
-        img: camovoDrvoImg,
-        text: [CAMOVO_DRVO, CAMOVO_DRVO_2, CAMOVO_DRVO_3],
-        sideList: [
-            'Troslojno limelirana čamovina',
-            'Profil 68/81',
-            'AGB okovi aluminijumska okapnica na štoku prozora',
-            'Termo staklo 4/16/4',
-            '2 dihtung gume',
-        ],
-    },
+  'camovo-drvo': {
+    pageTitle: 'Čamovo drvo',
+    title: 'Asortiman naših proizvoda',
+    titleSecond: 'Dokazano je',
+    img: camovoDrvoImg,
+    imgSecond: camovoDrvoImg2,
+    text: [CAMOVO_DRVO, CAMOVO_DRVO_2, CAMOVO_DRVO_3],
+    sideList: [
+      'Troslojno limelirana čamovina',
+      'Profil 68/81',
+      'AGB okovi aluminijumska okapnica na štoku prozora',
+      'Termo staklo 4/16/4',
+      '2 dihtung gume',
+    ],
+  },
 };

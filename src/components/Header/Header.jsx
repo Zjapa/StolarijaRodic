1cc6125c613c6c840b50eed3e9f2f121f7ecd75e
Zjapa <japax97@gmail.com>
2023-08-11 14:09:14 +0200

    Changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,36 +9,36 @@ import NavTab from './NavTab/NavTab';
 import './Header.scss';
 
 const Header = () => {
-    const [toggleMenu, setToggleMenu] = useState(false);
-    const handleToggleMenu = () => {
-        setToggleMenu((prevToggle) => !prevToggle);
-    };
-    return (
-        <header>
-            <HeaderTop />
-            <div className="header-bottom">
-                <div className="container header-bottom-container">
-                    <Image src={logo} width="100" height="40" alt="Stolarija Rodic Logo" className="logo" />
-                    <nav className={`nav-links  ${toggleMenu ? 'toggle' : ''}`}>
-                        {navigationConfig.map((navLink) => (
-                            <NavTab
-                                key={navLink.title}
-                                dropdown={navLink.dropdown}
-                                url={navLink.url}
-                                title={navLink.title}
-                                handleToggleMenu={handleToggleMenu}
-                            />
-                        ))}
-                    </nav>
-                    <div className="menu" onClick={handleToggleMenu}>
-                        <span className="line1"></span>
-                        <span className="line2"></span>
-                        <span className="line3"></span>
-                    </div>
-                </div>
-            </div>
-        </header>
-    );
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const handleToggleMenu = () => {
+    setToggleMenu((prevToggle) => !prevToggle);
+  };
+  return (
+    <header>
+      <HeaderTop />
+      <div className="header-bottom">
+        <div className=" header-bottom-container">
+          <Image src={logo} width="100" height="40" alt="Stolarija Rodic Logo" className="logo" />
+          <nav className={`nav-links  ${toggleMenu ? 'toggle' : ''}`}>
+            {navigationConfig.map((navLink) => (
+              <NavTab
+                key={navLink.title}
+                dropdown={navLink.dropdown}
+                url={navLink.url}
+                title={navLink.title}
+                handleToggleMenu={handleToggleMenu}
+              />
+            ))}
+          </nav>
+          <div className="menu" onClick={handleToggleMenu}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/components/Header/nav-const.js
+++ src/components/Header/nav-const.js
@@ -1,56 +1,57 @@
 export const navigationConfig = [
-    {
-        title: 'Početna',
-        url: '/',
-    },
-    {
-        title: 'O nama',
-        url: '/o-nama',
-    },
-    {
-        title: 'Drvena stolarija',
-        url: '',
-        dropdown: [
-            { title: 'Drveni prozori - čamovo drvo', url: 'camovo-drvo' },
-            { title: 'Drvo - Aluminijum stolarija', url: 'drvo-aluminijum' },
-            { title: 'Drveni kapak', url: 'drveni-kapak' },
-            { title: 'Ulazna vrata - puno drvo', url: 'ulazna-vrata-puno-drvo' },
-        ],
-    },
-    {
-        title: 'Unutrašnja vrata',
-        url: '',
-        dropdown: [
-            { title: 'Medijapan - folija', url: 'medijapan-folija' },
-            { title: 'Medijapan - farbani', url: 'medijapan-farbani' },
-            { title: 'Medijapan - furnirani', url: 'medijapan-furnirani' },
-            { title: 'Unutrašnja vrata - puno drvo', url: 'unutrasnja-vrata-puno-drvo' },
-        ],
-    },
-    {
-      title: 'Velux krovni prozori',
-      url: '',
-      dropdown: [
-          { title: 'Velux', url: 'velux-ovlasceni-prodavac' },
-          { title: 'Standard krovni prozori - drvo', url: 'velux-standard-drvo' },
-          { title: 'Standard krovni prozori - beli', url: 'velux-standard-beli'},
-          { title: 'Standard plus krovni prozori - drvo', url: 'velux-standard-plus-drvo' },
-          { title: 'Standard plus krovni prozori - beli', url: 'velux-standard-plus-beli' },
-          { title: 'Unutrašnje roletne - zaštita od svetlosti', url: 'velux-zastita-svetlost' },
-          { title: 'Spoljašnje mrežice - zaštita od toplote', url: 'velux-zastita-toplota' },
-      ],
+  {
+    title: 'Početna',
+    url: '/',
   },
-    {
-        title: 'PVC Stolarija',
-        url: '/pvc-stolarija',
-    },
-    {
-        title: 'Projekti',
-        url: '/projekti',
-    },
+  {
+    title: 'O nama',
+    url: '/o-nama',
+  },
+  {
+    title: 'Drvena stolarija',
+    url: '',
+    dropdown: [
+      { title: 'Drveni prozori - čamovo drvo', url: 'camovo-drvo' },
 
-    {
-        title: 'Kontakt',
-        url: '/kontakt',
-    },
+      { title: 'Drveni kapak', url: 'drveni-kapak' },
+      { title: 'Ulazna vrata - puno drvo', url: 'ulazna-vrata-puno-drvo' },
+    ],
+  },
+  { title: 'Drvo - Aluminijum', url: 'drvo-aluminijum' },
+  {
+    title: 'Unutrašnja vrata',
+    url: '',
+    dropdown: [
+      { title: 'Medijapan - folija', url: 'medijapan-folija' },
+      { title: 'Medijapan - farbani', url: 'medijapan-farbani' },
+      { title: 'Medijapan - furnirani', url: 'medijapan-furnirani' },
+      { title: 'Unutrašnja vrata - puno drvo', url: 'unutrasnja-vrata-puno-drvo' },
+    ],
+  },
+  {
+    title: 'Velux krovni prozori',
+    url: '',
+    dropdown: [
+      { title: 'Velux', url: 'velux-ovlasceni-prodavac' },
+      { title: 'Standard krovni prozori - drvo', url: 'velux-standard-drvo' },
+      { title: 'Standard krovni prozori - beli', url: 'velux-standard-beli' },
+      { title: 'Standard plus krovni prozori - drvo', url: 'velux-standard-plus-drvo' },
+      { title: 'Standard plus krovni prozori - beli', url: 'velux-standard-plus-beli' },
+      { title: 'Unutrašnje roletne - zaštita od svetlosti', url: 'velux-zastita-svetlost' },
+      { title: 'Spoljašnje mrežice - zaštita od toplote', url: 'velux-zastita-toplota' },
+    ],
+  },
+  {
+    title: 'PVC Stolarija',
+    url: '/pvc-stolarija',
+  },
+  {
+    title: 'Projekti',
+    url: '/projekti',
+  },
+
+  {
+    title: 'Kontakt',
+    url: '/kontakt',
+  },
 ];

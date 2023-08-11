a8677c9a1695acb2b7eae6536dafc11204677175
Zjapa <japax97@gmail.com>
2023-08-11 16:34:14 +0200

    Click on bg close LightBox

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -6,45 +6,45 @@ import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
 import LightBox from './LightBox/LightBox';
 
 const Gallery = ({ photos }) => {
-    const [lightBoxImage, setLightBoxImage] = useState('');
-    const [imgWidth, setImageWidth] = useState(0);
+  const [lightBoxImage, setLightBoxImage] = useState('');
+  const [imgWidth, setImageWidth] = useState(0);
 
-    const carouselRef = useRef();
-    const imgRef = useRef();
+  const carouselRef = useRef();
+  const imgRef = useRef();
 
-    const handleNav = (arrow) => {
-        carouselRef.current.scrollLeft += arrow === 'left' ? -imgWidth : imgWidth;
-    };
+  const handleNav = (arrow) => {
+    carouselRef.current.scrollLeft += arrow === 'left' ? -imgWidth : imgWidth;
+  };
 
-    const handleOpenLightbox = (img) => {
-        setLightBoxImage(img);
-    };
+  const handleOpenLightbox = (img) => {
+    setLightBoxImage(img);
+  };
 
-    useEffect(() => {
-        setImageWidth(imgRef.current.clientWidth + 14);
-    }, [imgRef]);
+  useEffect(() => {
+    setImageWidth(imgRef.current.clientWidth + 14);
+  }, [imgRef]);
 
-    return (
-        <div className="wrapper">
-            <FaAngleLeft id="left" className="icon-left" onClick={() => handleNav('left')} />
-            <div ref={carouselRef} className="carousel">
-                {photos.map((photo) => (
-                    <Image
-                        ref={imgRef}
-                        key={photo.title}
-                        src={photo.img}
-                        alt={photo.title}
-                        width={300}
-                        height={300}
-                        className="gallery-img"
-                        onClick={() => handleOpenLightbox(photo.img)}
-                    />
-                ))}
-            </div>
-            <FaAngleRight id="right" className="icon-right" onClick={handleNav} />
-            {lightBoxImage && <LightBox img={lightBoxImage} />}
-        </div>
-    );
+  return (
+    <div className="wrapper">
+      <FaAngleLeft id="left" className="icon-left" onClick={() => handleNav('left')} />
+      <div ref={carouselRef} className="carousel">
+        {photos.map((photo) => (
+          <Image
+            ref={imgRef}
+            key={photo.title}
+            src={photo.img}
+            alt={photo.title}
+            width={300}
+            height={300}
+            className="gallery-img"
+            onClick={() => handleOpenLightbox(photo.img)}
+          />
+        ))}
+      </div>
+      <FaAngleRight id="right" className="icon-right" onClick={handleNav} />
+      {lightBoxImage && <LightBox img={lightBoxImage} closeLightbox={handleOpenLightbox} />}
+    </div>
+  );
 };
 
 export default Gallery;
--- src/components/Gallery/LightBox/LightBox.jsx
+++ src/components/Gallery/LightBox/LightBox.jsx
@@ -3,21 +3,25 @@ import React, { useEffect, useState } from 'react';
 import './LightBox.scss';
 import { createPortal } from 'react-dom';
 
-const LightBox = ({ img }) => {
-    const [mounted, setMounted] = useState(false);
+const LightBox = ({ img, closeLightbox }) => {
+  const [mounted, setMounted] = useState(false);
 
-    useEffect(() => setMounted(true), []);
+  useEffect(() => setMounted(true), []);
 
-    return mounted
-        ? createPortal(
-              <div id='background' className="background" onClick={(e) => console.log(e.target.id)}>
-                  <div className="image-container">
-                      <Image src={img} alt="Uvecana slika iz galerije" className="lightbox-img" width={300} height={300}/>
-                  </div>
-              </div>,
-              document.body
-          )
-        : null;
+  const handleClose = (e) => {
+    if (e.target.id === 'background') closeLightbox('');
+  };
+
+  return mounted
+    ? createPortal(
+        <div id="background" className="background" onClick={handleClose}>
+          <div className="image-container">
+            <Image src={img} alt="Uvecana slika iz galerije" className="lightbox-img" width={300} height={300} />
+          </div>
+        </div>,
+        document.body
+      )
+    : null;
 };
 
 export default LightBox;
